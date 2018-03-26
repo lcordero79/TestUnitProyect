732babb9a5911658910f5523419f03055e04cec6
lcordero <luiscordero@outlook.com>
2018-03-26 11:20:09 -0300

    Test Unit Registro de Usuario VS and MS SQL

---
+++ WebRegistroUsuario/Models/SingInTestsRegitroUsuario.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVC_Parte_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Parte_1.Models.Testsnuevouser
+{
+    [TestClass()]
+    public class SingInTestsRegitroUsuario2
+    {
+        [TestMethod()]
+        public void singInTestRegistroUserExist()
+        {
+            //Arrange
+            var RegUser = new SingIn();//{ User = "lcordero", Email = "Lcordero@gmail.com" };
+            RegUser.User = "Lcordero";
+            RegUser.Email = "Lcordero@gmail.com";
+
+            //Act
+            var resultado = RegUser.singIn();
+
+            //Assert
+            Assert.IsFalse(resultado);
+            
+        }
+
+        [TestMethod()]
+        public void singInTestRegistroUserNoExist()
+        {
+            //Arrange
+            var RegUser = new SingIn();//{ User = "lcordero", Email = "Lcordero@gmail.com" };
+            RegUser.Name = "Raúl";
+            RegUser.LastName = "González";
+            RegUser.User = "Rgonzalez";
+            RegUser.Email = "Rgonzalez@gmail.com";
+            RegUser.Password = "123456";
+
+            //Act
+            var resultado = RegUser.singIn();
+
+            //Assert
+            Assert.IsTrue(resultado);
+        }
+    }
+}
---
+++ WebRegistroUsuario/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WebRegistroUsuario")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebRegistroUsuario")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bafb7b3e-e49f-46bd-9129-36b1a7db90c8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
